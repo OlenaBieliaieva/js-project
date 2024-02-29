7e6a709e6fe09331bcf4ab6e7c8353e48f2c9a02
OlenaBieliaieva <144038135+OlenaBieliaieva@users.noreply.github.com>
2024-02-29 17:12:41 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,91 @@
+const charityFunds = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: './img/save-the-children.png',
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: './img/project-hope.png',
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: './img/united24.png',
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: './img/international-medical-corps.png',
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: './img/medicines-sans-frontieres.png',
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: './img/razom.png',
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: './img/action-against-hunger.png',
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: './img/world-vision.png',
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: './img/sergiy-prytula.png',
+  },
+];
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+const charityElem = document.querySelector('.support-funds-list');
+
+function fundsMarkup() {
+  const result = charityFunds
+    .map(({ title, url, img }) => {
+      return `<li support-funds-list-link">
+            <a class="support-funds-list-link-image-link" href="${url}">
+                <img class="support-funds-list-link-image" src="${img}"
+                    alt="${title} target="_blank"" 
+            </a>
+        </li>`;
+    })
+    .join('');
+  charityElem.innerHTML = result;
+}
+fundsMarkup();
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
